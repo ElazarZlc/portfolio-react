ebc7631be54d8f7db0e676f91af88703d7bcf2e8
Elazar Zlc <elazal1991@gmail.com>
2024-05-30 15:42:37 +0300

    add components to navbar and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Tech, Works, StarsCanvas } from './components'
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Tech,
+  Works,
+  StarsCanvas,
+  Navbar,
+} from "./components";
 export default function App() {
-
   return (
     <>
-
-    <BrowserRouter>
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-patten bg-cover'></div>
-    </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-patten bg-cover bg-no-reapat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
