25e4b1d1de81601064e91ee0a2aeef562a47cb49
Elazar Zlc <elazal1991@gmail.com>
2024-06-02 17:02:16 +0300

    navlink design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
  import {logo, menu, close } from "../assets"
 const Navbar = () => {
   const [active, setActive] = useState("")
+  
   return (
-    <nav className={`${styles.paddindx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/"
         className="flex items-center gap-2" onClick={() => {
@@ -16,7 +17,17 @@ const Navbar = () => {
           <img src={logo}  alt="logo"  className='w-9 h-9 object-contain'/>
           <p className='text-white text-[18px] font-bolt cursor-pointer'>Elazar <span className='sm:block hidden'>| javaScript Mastery </span> </p>
           </Link>
-          <p className='text-red-500'>asdsa</p>
+          <ul className='list-none hodden sm:flex flex-row gap-10'>
+            {navLinks.map((link) => (
+              <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            ><a href={`#${link.id}`}>{link.title}</a></li>
+            ))}
+          </ul>
       </div>
     </nav>
   )
