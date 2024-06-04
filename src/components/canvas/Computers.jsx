3ef20745a4acd6bffda5d7311f80ca2811e46846
Elazar Zlc <elazal1991@gmail.com>
2024-06-04 13:48:29 +0300

    fix a letter

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import {suspense, useEffect, useState} from 'react'
 import {Canvas} from '@react-three/fiber'
-import {orbitControls, Preload, useGLTF} from '@react-three/drei'
+import {OrbitControls, Preload, useGLTF} from '@react-three/drei'
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
